5b286690235c98678ed3ca6c8eac00daf87ee125
px <479889183@qq.com>
2016-12-24 13:37:47 +0800

    第一次提交

---
+++ DialogDemo/Assets/scripts/showDialog.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class showDialog : MonoBehaviour {
+    public GameObject panel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    /// <summary>
+    /// 显示dialog
+    /// </summary>
+    public void sDialog() 
+    {     
+        panel.SetActive(true);
+    }
+    /// <summary>
+    /// 关闭dialog
+    /// </summary>
+    public void cDialog()
+    {
+        panel.SetActive(false);
+    }
+}
